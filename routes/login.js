1aa6a2da8fc7b52aa131bd8ae06f3d69ae2b9aff
HeSheng09 <1153508987@qq.com>
2021-03-12 00:46:10 +0800

    get params in ejs

--- routes/login.js
+++ routes/login.js
@@ -1,31 +1,41 @@
 var express = require('express');
+var request=require('request')
 var router = express.Router();
 
 // 跳转到login页面
-router.get("/",function (req,res,netx) {
+router.get("/", function (req, res, netx) {
     res.render("login");
 })
 
 // 以post方式提交登录信息
-router.post('/commit',function (req,res,next) {
-    let params=req.body;
-    if(params.user==="张三" && params.password==="password"){
-        req.session.user="张三";
-        res.redirect("/student")
-        // res.render('student',{user: params.user});
-        // res.send("登录成功");
-    }else{
-        res.send("登录失败");
-    }
+router.post('/commit', function (req, res, next) {
+    let params = req.body;
+    console.log(params)
+    request.post({
+        url: 'http://localhost:8080/server/user/login',
+        form: {name: params.username, pwd: params.pwd}
+    }, function (error, response, body) {
+        // 返回的结果和 GET 一样
+        if (!error && response.statusCode === 200) {
+            let data = JSON.parse(body).data;
+            console.log(data)
+            req.session.user = data.id;
+            req.session.role = data.role;
+            res.render("student", {id: data.id, name: data.name, password: data.pwd, role: data.role})
+        } else {
+            console.log(error)
+            res.send('error')
+        }
+    })
 })
 
 // 以get方式提交登录信息
-router.get('/commit',function (req,res,next) {
-    let params=req.query || req.params;
-    if(params.user==="张三" && params.password==="password"){
-        res.session.user="张三";
+router.get('/commit', function (req, res, next) {
+    let params = req.query || req.params;
+    if (params.user === "张三" && params.password === "password") {
+        res.session.user = "张三";
         res.redirect("/student");
-    }else{
+    } else {
         res.send("登录失败");
     }
 })
--- routes/student.js
+++ routes/student.js
@@ -1,10 +1,42 @@
 var express = require('express');
 // var cookieParser=require('cookie-parser');
+var request = require('request')
 var router = express.Router();
 
 // 跳转到学生首页
-router.get('/',function (req,res,next) {
-    res.render('student',{user:req.session.user});
+router.get('/', function (req, res, next) {
+    res.render('student', {user: req.session.user});
+})
+
+router.post('/get_user_info', function (req, res, next) {
+    let params = req.body;
+    console.log(params)
+    request.post({
+        url: 'http://localhost:8080/server/user/user_info',
+        form: {user_id: params.user_id}
+    }, function (error, response, body) {
+        // 返回的结果和 GET 一样
+        if (!error && response.statusCode === 200) {
+            let user = JSON.parse(body).data;
+            if (req.session.role) {
+                //ns
+                user = {role: req.session.role, name: user.ns_name, tel: user.tel, email: user.email}
+            } else {
+                //student
+                user = {
+                    role: req.session.role,
+                    name: user.stu_name,
+                    tel: user.tel,
+                    email: user.email,
+                    addres: user.address
+                }
+            }
+            res.send(user)
+        } else {
+            console.log(error)
+            res.send('error')
+        }
+    })
 })
 
 module.exports = router;
