823a5c3687ad20c79f44b8f3ede2f8fd9e4f5ef5
ma yan <570275364@qq.com>
2021-03-11 22:17:59 +0800

    research2

--- routes/event6.js
+++ routes/event6.js
@@ -13,7 +13,23 @@ router.get('/:eventid',function (req,res,next) {
     console.log("wolaile!!!!")
     req.session.eventid=req.params.eventid;
     console.log(req.session);
-    res.render('event6',{userid:req.session.userid,role:req.session.role,eventid:req.session.eventid});
+    if(req.session.role==="false"){
+        console.log("是学生");
+        res.render('event6',{userid:req.session.userid,role:req.session.role,eventid:req.session.eventid});
+        }
+})
+router.get('/:eventid/:eventkind',function (req,res,next) {
+    console.log("wolaile!!!!")
+    req.session.eventid=req.params.eventid;
+    req.session.eventkind=req.params.eventkind
+    console.log(req.session);
+    if(req.session.role==="false"){
+        console.log("是学生");
+    }
+    else{
+        console.log("是管理人员！");
+        res.render('NTUevent6',{userid:req.session.userid,role:req.session.role,eventid:req.session.eventid,eventkind:req.session.eventkind});
+    }
 })
 
 // router.post('/',function (req,res,next) {
--- routes/personal.js
+++ routes/personal.js
@@ -4,7 +4,14 @@ var router = express.Router();
 
 // 跳转到搜索页
 router.get('/',function (req,res,next) {
-    res.render('personal',{userid:req.session.userid,role:req.session.role});
+    console.log(req.session.role);
+    if(req.session.role==="false"){
+        console.log("是学生");
+    res.render('personal',{userid:req.session.userid,role:req.session.role});}
+    else{
+        console.log("是管理人员！");
+        res.render('NTUpersonal',{userid:req.session.userid,role:req.session.role});
+    }
 })
 
 module.exports = router;
